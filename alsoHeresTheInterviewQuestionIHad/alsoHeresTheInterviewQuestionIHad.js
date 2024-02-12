455516bb0bfdf646c8fbffba97fb9bf792a1847c
IvarForkbeard <stephen@garstin.ca>
2024-02-12 16:10:23 -0500

    Minor cleanup.  Still puzzling out not recalculating every shift.

--- alsoHeresTheInterviewQuestionIHad/alsoHeresTheInterviewQuestionIHad.js
+++ alsoHeresTheInterviewQuestionIHad/alsoHeresTheInterviewQuestionIHad.js
@@ -1,42 +1,43 @@
 const fs = require("fs");
 const streetArray = fs.readFileSync("data.txt", "utf-8").split(",");
 
-let answer = "";
+const streetArraySorted = streetArray.sort(function (a, b) {
+    return a - b;
+});
 
 let numberOfBuildings = streetArray.length;
+
 let tallestBuildingSoFar = 0;
 let tallestBuilding = Math.max(...streetArray);
 
-let bannerOneHeight = tallestBuilding;
-let bannerOneWidth = numberOfBuildings;
+let bannerOneHeight = 0;
+let bannerOneWidth = 0;
 let bannerOneArea = tallestBuilding * numberOfBuildings;
-let bannerTwoHeight = tallestBuilding;
-let bannerTwoWidth = numberOfBuildings;
-let bannerTwoArea = tallestBuilding * numberOfBuildings;
+let bannerTwoHeight = 0;
+let bannerTwoWidth = 0;
+let bannerTwoArea = 0;
 
-let bestArea = bannerOneArea;
+function findBestArea() {
+    let bestAreaInFunction = bannerOneArea;
+    for (let i = 0; i < numberOfBuildings - 1; i++) {
+        if (streetArray[i] > tallestBuildingSoFar) {
+            tallestBuildingSoFar = streetArray[i];
+        }
 
-function updateBanners(address){
-    bannerOneHeight = tallestBuildingSoFar
-    bannerOneWidth = address + 1;
-    bannerOneArea = bannerOneHeight * bannerOneWidth;
-    bannerTwoHeight = Math.max(...streetArray.slice(address + 1, streetArray.length))
-    bannerTwoWidth = numberOfBuildings - address - 1
-    bannerTwoArea = bannerTwoHeight * bannerTwoWidth;
+        bannerOneHeight = tallestBuildingSoFar;
+        bannerOneWidth = i + 1;
+        bannerOneArea = bannerOneHeight * bannerOneWidth;
+        bannerTwoHeight = Math.max(...streetArray.slice(i + 1, streetArray.length));
+        bannerTwoWidth = numberOfBuildings - i - 1;
+        bannerTwoArea = bannerTwoHeight * bannerTwoWidth;
+
+        if (bannerOneArea + bannerTwoArea < bestAreaInFunction) {
+            bestAreaInFunction = bannerOneArea + bannerTwoArea;
+        }
+    }
+    return bestAreaInFunction;
 }
 
-for (let i = 0; i < numberOfBuildings - 1; i++){
-
-    if (streetArray[i] > tallestBuildingSoFar) {
-        tallestBuildingSoFar = streetArray[i];
-    }
-    
-    updateBanners(i);
-    
-    if (bannerOneArea+ bannerTwoArea < bestArea) {
-        bestArea = bannerOneArea + bannerTwoArea;
-    }
-}
-
-answer = "The best area we can cover is " + bestArea;
+let bestArea = findBestArea();
+let answer = "The best area we can cover is " + bestArea;
 console.log(answer);
