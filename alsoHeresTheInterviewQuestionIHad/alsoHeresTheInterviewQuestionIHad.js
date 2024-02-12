1e7b532548e026658f344313de15bdd11e6973e0
IvarForkbeard <stephen@garstin.ca>
2024-02-12 16:55:03 -0500

    Hopefully getting closer, still a concept to grasp.

--- alsoHeresTheInterviewQuestionIHad/alsoHeresTheInterviewQuestionIHad.js
+++ alsoHeresTheInterviewQuestionIHad/alsoHeresTheInterviewQuestionIHad.js
@@ -1,24 +1,21 @@
 const fs = require("fs");
-const streetArray = fs.readFileSync("data.txt", "utf-8").split(",");
+const buildings = fs.readFileSync("data.txt", "utf-8").split(",");
 
-const streetArraySorted = streetArray.sort(function (a, b) {
-    return a - b;
-});
+function bestArea(streetArray) {
+    let numberOfBuildings = streetArray.length;
 
-let numberOfBuildings = streetArray.length;
+    let tallestBuildingSoFar = 0;
+    let tallestBuilding = Math.max(...streetArray);
 
-let tallestBuildingSoFar = 0;
-let tallestBuilding = Math.max(...streetArray);
+    let bannerOneHeight = 0;
+    let bannerOneWidth = 0;
+    let bannerOneArea = tallestBuilding * numberOfBuildings;
+    let bannerTwoHeight = 0;
+    let bannerTwoWidth = 0;
+    let bannerTwoArea = 0;
 
-let bannerOneHeight = 0;
-let bannerOneWidth = 0;
-let bannerOneArea = tallestBuilding * numberOfBuildings;
-let bannerTwoHeight = 0;
-let bannerTwoWidth = 0;
-let bannerTwoArea = 0;
-
-function findBestArea() {
     let bestAreaInFunction = bannerOneArea;
+
     for (let i = 0; i < numberOfBuildings - 1; i++) {
         if (streetArray[i] > tallestBuildingSoFar) {
             tallestBuildingSoFar = streetArray[i];
@@ -38,6 +35,4 @@ function findBestArea() {
     return bestAreaInFunction;
 }
 
-let bestArea = findBestArea();
-let answer = "The best area we can cover is " + bestArea;
-console.log(answer);
+console.log("The best area we can cover is " + bestArea(buildings));
